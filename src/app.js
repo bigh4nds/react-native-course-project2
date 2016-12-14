fc56b953110fe77944ec2496d7da15062db1cd9a
Chris Finn <christopherpatrickfinn@gmail.com>
2016-12-14 00:51:47 -0500

    issue with logout button & spinner placement

---
+++ src/app.js
@@ -0,0 +1,53 @@
+import firebase from 'firebase';
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import { Header, Button, Spinner } from './components/common';
+import LoginForm from './components/LoginForm';
+
+class App extends Component {
+	state = { loggedIn: null, containerStyle: null };
+
+	componentWillMount() {
+		firebase.initializeApp({
+			apiKey: '',
+			authDomain: '',
+			databaseURL: '',
+			storageBucket: '',
+			messagingSenderId: ''
+		});
+
+		firebase.auth().onAuthStateChanged((user) => {
+			if (user) {
+				this.setState({ loggedIn: true });
+			} else {
+				this.setState({ loggedIn: false });
+			}
+		});
+	}
+
+	renderContent() {
+		switch (this.state.loggedIn) {
+			case true: 
+				return (
+					<Button onPress={() => firebase.auth().signOut()} >
+						Log Out
+					</Button>
+				);
+			case false:
+				return <LoginForm />;
+			default:
+				return <Spinner size="large" />;
+		}
+	}
+
+	render() {
+		return (
+			<View style={this.containerStyle}>
+				<Header headerText="Authentication" />
+				{this.renderContent()}
+			</View>
+		);
+	}
+}
+
+export default App;
